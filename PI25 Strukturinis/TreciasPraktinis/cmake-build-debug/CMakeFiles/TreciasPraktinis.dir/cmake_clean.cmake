00515f3cbb765a46411230b564764044edf60fd8
Tautvydas Bagdžius <s057522@ad.viko.lt>
2025-11-06 09:17:32 +0200

    copypasta

---
+++ PI25 Strukturinis/TreciasPraktinis/cmake-build-debug/CMakeFiles/TreciasPraktinis.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TreciasPraktinis.dir/main.cpp.obj"
+  "CMakeFiles/TreciasPraktinis.dir/main.cpp.obj.d"
+  "TreciasPraktinis.exe"
+  "TreciasPraktinis.exe.manifest"
+  "TreciasPraktinis.pdb"
+  "libTreciasPraktinis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TreciasPraktinis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
