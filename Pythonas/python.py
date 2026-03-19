d44741db2131da9d95ec987f0a438607155332f0
Tautvydas Bagdžius <s057522@ad.viko.lt>
2026-03-19 13:12:33 +0200

    copypasta

---
+++ Pythonas/python.py
@@ -0,0 +1,15 @@
+name = "Jonas"
+age = 20
+grades = [8, 9, 10]
+float = 8.4
+print(name, age, grades, float)
+name = "Andrius"
+age = 24
+grades = [4, 7, 10]
+float = 9.1
+print(name, age, grades, float)
+name = "Karolis"
+age = 18
+grades = [8, 9, 10]
+float = 7.5
+print(name, age, grades, float)
