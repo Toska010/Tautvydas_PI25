d4c780138423d68be15cb5d6240b11040a733001
Tautvydas Bagdžius <toska.010@gmail.com>
2026-05-15 10:39:45 +0300

    copypasta

---
+++ Tautvydas_Bagdzius_PI25_K4_Programa1/cmake-build-debug/CMakeFiles/Tautvydas_Bagdzius_PI25_K4_Programa1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tautvydas_Bagdzius_PI25_K4_Programa1.dir/main.cpp.obj"
+  "CMakeFiles/Tautvydas_Bagdzius_PI25_K4_Programa1.dir/main.cpp.obj.d"
+  "Tautvydas_Bagdzius_PI25_K4_Programa1.exe"
+  "Tautvydas_Bagdzius_PI25_K4_Programa1.exe.manifest"
+  "Tautvydas_Bagdzius_PI25_K4_Programa1.pdb"
+  "libTautvydas_Bagdzius_PI25_K4_Programa1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tautvydas_Bagdzius_PI25_K4_Programa1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Tautvydas_Bagdzius_PI25_K4_Programa1/main.cpp
+++ Tautvydas_Bagdzius_PI25_K4_Programa1/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 bool yraMasyvas = false;
-
 struct sarasas {
     int skaicius;
     sarasas *kitas;
 };
 sarasas *pradzia = nullptr, *pabaiga = nullptr, *elem = nullptr, *trintukas = nullptr;
-
 struct medis {
     int skaicius;
     int balansas;
@@ -22,7 +20,7 @@ medis* naujas_elementasMedzio(int perkeltas) {
     el -> kaire = nullptr;
     el -> desine = nullptr;
     return el;
-}
+}//DONE
 int aukstis (medis* virsus) {
     if (virsus == nullptr) {
         return 0;
@@ -37,7 +35,7 @@ int aukstis (medis* virsus) {
             return 1 + desine;
         }
     }
-}
+}//DONE
 int skaiciuotiBalansa(medis* virsus) {
     if (virsus == nullptr) {
         return 0;
@@ -45,22 +43,7 @@ int skaiciuotiBalansa(medis* virsus) {
     else {
         return aukstis(virsus -> desine) - aukstis (virsus -> kaire);
     }
-}
-medis* iterpti_iMedi(medis* virsus, int perduotas) {
-    if (virsus == nullptr) {
-        return naujas_elementasMedzio(perduotas);
-    }
-    if (virsus -> kaire == nullptr) {
-        virsus -> kaire = naujas_elementasMedzio(perduotas);
-        return virsus;
-    }
-    if (virsus -> desine == nullptr) {
-        virsus -> desine = naujas_elementasMedzio(perduotas);
-        return virsus;
-    }
-    iterpti_iMedi(virsus -> kaire, perduotas);
-    return virsus;
-}
+}//DONE
 medis* desinen(medis* virsus) {
     if (virsus == NULL || virsus->kaire == NULL) {
         return virsus;
@@ -73,7 +56,7 @@ medis* desinen(medis* virsus) {
     virsus->balansas = skaiciuotiBalansa(virsus);
     elem->balansas = skaiciuotiBalansa(elem);
     return elem;
-}
+}//DONE
 medis* kairen(medis* virsus) {
     if (virsus == NULL || virsus->desine == NULL) {
         return virsus;
@@ -86,8 +69,52 @@ medis* kairen(medis* virsus) {
     virsus->balansas = skaiciuotiBalansa(virsus);
     elem->balansas = skaiciuotiBalansa(elem);
     return elem;
-}
-void spausdinti_Medi(){}
+}//DONE
+medis* iterpti_iMedi(medis* virsus, int perduotas) {
+    if (virsus == nullptr) {
+        return naujas_elementasMedzio(perduotas);
+    }
+    if (perduotas < virsus -> skaicius) {
+        virsus -> kaire = iterpti_iMedi(virsus -> kaire, perduotas);
+    }
+    else {
+        virsus -> desine = iterpti_iMedi(virsus -> desine, perduotas);
+    }
+    virsus -> balansas = skaiciuotiBalansa(virsus);
+    if (virsus -> balansas < -1) {
+        if (perduotas < virsus -> kaire -> skaicius) {
+            return desinen(virsus);
+        }
+    }
+    else {
+        virsus -> kaire = kairen(virsus -> kaire);
+        return desinen(virsus);
+    }
+    if (virsus -> balansas > 1) {
+        if (perduotas >= virsus -> desine -> skaicius) {
+            return kairen(virsus);
+        }
+        else {
+            virsus -> desine = desinen(virsus -> desine);
+            return kairen(virsus);
+        }
+    }
+    return virsus;
+}//DONE
+void spausdinti_Medi_Kartojimas(medis* virsus) {
+    if (virsus == nullptr) return;
+    spausdinti_Medi_Kartojimas(virsus->kaire);
+    cout << virsus->skaicius << " ";
+    spausdinti_Medi_Kartojimas(virsus->desine);
+}//DONE
+void spausdinti_Medi() {
+    if (virsus == nullptr) {
+        cout << "Medis tuscias." << endl;
+        return;
+    }
+    spausdinti_Medi_Kartojimas(virsus);
+    cout << endl;
+}//DONE
 void vienkryptis_Sarasas(int sk) {
     if (yraMasyvas == false) {
         elem = new sarasas();
@@ -112,50 +139,34 @@ void iterpti_Elementa() {
     vienkryptis_Sarasas(skaicius);
 }//DONE
 void pasalinti_Elementa() {
+
     cout << "Iveskite skaiciu salinimui: " << endl;
     int skaicius;
     cin >> skaicius;
     trintukas = pradzia;
-    bool yraIeskomas = false;
-    while (trintukas != nullptr) {
-        if (trintukas -> skaicius == skaicius) {
-            yraIeskomas = true;
-            if (trintukas == pradzia) {
-                if (trintukas -> kitas != nullptr) {
-                    pradzia = trintukas -> kitas;
-                    delete trintukas;
-                    break;
-                }
-                else {
-                    delete trintukas;
-                    pradzia = nullptr;
-                    pabaiga = nullptr;
-                    trintukas = nullptr;
-                    elem = nullptr;
-                    break;
-                }
-            }
-            else {
-                elem = pradzia;
-                while (elem -> kitas != trintukas) {
-                    elem = elem -> kitas;
-                }
-                if (trintukas -> kitas == nullptr) {
-                    elem -> kitas = nullptr;
-                }
-                else {
-                    elem -> kitas = trintukas -> kitas;
-                }
-            }
-        }
+    elem = nullptr;
+    while (trintukas != nullptr && trintukas -> skaicius != skaicius) {
+        elem = trintukas;
         trintukas = trintukas -> kitas;
     }
-    if (yraIeskomas == true) {
-        cout << "Skaicius " << skaicius << " pasalintas." << endl;
-    }
-    else {
+    if (trintukas == nullptr) {
         cout << "Skaicius " << skaicius << " nerastas!" << endl;
+        return;
     }
+    if (elem == nullptr) {
+        pradzia = trintukas -> kitas;
+    } else {
+        elem -> kitas = trintukas -> kitas;
+    }
+    if (trintukas == pabaiga) {
+        pabaiga = elem;
+    }
+    delete trintukas;
+    if (pradzia == nullptr) {
+        pabaiga = nullptr;
+        yraMasyvas = false;
+    }
+    cout << "Skaicius " << skaicius << " pasalintas." << endl;
 }//DONE
 void spausdinti_Sarasa() {
     elem = pradzia;
@@ -184,9 +195,75 @@ void rasti_Skaiciu() {
         cout << "Skaicius " << skaicius << " nerastas!" <<  endl;
     }
 }//DONE
-void perkelti_Elementus(){}
+void perkelti_Elementus() {
+    elem = pradzia;
+    while (elem != nullptr) {
+        virsus = iterpti_iMedi(virsus, elem -> skaicius);
+        trintukas = elem;
+        elem = elem -> kitas;
+        delete trintukas;
+    }
+    pradzia = nullptr;
+    pabaiga = nullptr;
+    elem = nullptr;
+    trintukas = nullptr;
+    yraMasyvas = false;
+    cout << "Visi saraso elementai perkelti i AVL medi." << endl;
 
-
+}//DONE
+void aprasymas_meniu() {
+    cout << "Pasirinkite funkcija:\n"
+            "0: Nutraukti programa\n"
+            "1: Iterpti skaiciu i sarasa\n"
+            "2: Pasalinti skaiciu is saraso\n"
+            "3: Spausdinti sarasa\n"
+            "4: Rasti skaiciu sarase\n"
+            "5: Perkelti saraso skaicius i AVL medi\n"
+            "6: Spausdinti AVL medi" << endl;
+}
+void pasirinkta_meniu() {
+    bool veikiam = true;
+    int pasirinkimas;
+    int krize = 0;
+    while (veikiam == true && krize < 5) {
+        aprasymas_meniu();
+        cin >> pasirinkimas;
+        switch (pasirinkimas) {
+            case 0: {
+                veikiam = false;
+                break;
+            }
+            case 1: {
+                iterpti_Elementa();
+                break;
+            }
+            case 2: {
+                pasalinti_Elementa();
+                break;
+            }
+            case 3: {
+                spausdinti_Sarasa();
+                break;
+            }
+            case 4: {
+                rasti_Skaiciu();
+                break;
+            }
+            case 5: {
+                perkelti_Elementus();
+                break;
+            }
+            case 6: {
+                spausdinti_Medi();
+                break;
+            }
+            default: {
+                cout << "Tokio pasirinkimo " << pasirinkimas << " nera!" << endl;
+                krize ++;
+            }
+        }
+    }
+}
 int main() {
-    return 0;
+    pasirinkta_meniu();
 }
